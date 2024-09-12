50b4c68526cdf07cb18233fb0837a97954e57bea
Adrian Ditmansen <adriandenutrolige@gmail.com>
2024-09-12 12:24:17 +0200

    ferdig Oppgave4

--- Oblig 2/Oppgave4.py
+++ Oblig 2/Oppgave4.py
@@ -1,6 +1,6 @@
 #Listen fra forrige oppgave
 tolkienSineBøker = ["Farmer Giles og Ham", "Lord of The rings The Fellowship of the Ring", "Lord of The rings The Return of the King", "Lord of The rings The Two Towers", "The Adventures of Tom Bombadil", "The Hobbit", "The Silmarillion", "Tree and Leaf", "Unfinished Tales"]
-
+tempListe = []
 søkeord = "Lord of The rings"
 
 #en måte å printe ut alle elementene som inneholder en stregn i lista
@@ -9,9 +9,10 @@ for Bok in tolkienSineBøker:
     if søkeord in Bok:
         print(Bok)
 
+#en annen måte å gjøre det på:
+for Bok in tolkienSineBøker:
+    if søkeord in Bok:
+        tempListe.append(Bok)
 
 
-
-
-
-
+print(tempListe)    

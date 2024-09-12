fb747cb6e005980eee16242aa8e89dc57225e77d
Adrian Ditmansen <adriandenutrolige@gmail.com>
2024-09-12 12:13:46 +0200

    oppgave4 gjrt rett, mangler for nr 2

--- Oblig 2/Oppgave4.py
+++ Oblig 2/Oppgave4.py
@@ -1,23 +1,17 @@
 #Listen fra forrige oppgave
 tolkienSineBøker = ["Farmer Giles og Ham", "Lord of The rings The Fellowship of the Ring", "Lord of The rings The Return of the King", "Lord of The rings The Two Towers", "The Adventures of Tom Bombadil", "The Hobbit", "The Silmarillion", "Tree and Leaf", "Unfinished Tales"]
 
-#tom liste
-tomListe = []
+søkeord = "Lord of The rings"
+
+#en måte å printe ut alle elementene som inneholder en stregn i lista
+for Bok in tolkienSineBøker:
+    
+    if søkeord in Bok:
+        print(Bok)
 
 
-#litt usikker på om jeg tolker oppgaven riktig, men
-tomListe.append(tolkienSineBøker[1])
-tomListe.append(tolkienSineBøker[2])
-tomListe.append(tolkienSineBøker[3])
 
 
-#en måte å printe ut alle elementene i lista
-for bøker in tomListe:
-    print(bøker)
 
-
-#en annen måte å kjøre det på
-for nummer in range(len(tomListe)):
-    print(tomListe[nummer])
 
 

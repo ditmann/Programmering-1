b17b37561f3f3ae08ffac73b4a706aa3f2cb2d7a
Adrian Ditmansen <adriandenutrolige@gmail.com>
2024-10-09 19:38:29 +0200

    ferdig med oppgave4

---
+++ Oblig 3/oppgave4.py
@@ -0,0 +1,11 @@
+print("dette er en volum kalkulator!")
+
+def volum():
+    lengde = float(input("hva er lengden?"))
+    bredde = float(input("hva er bredden?"))
+    høyde  = float(input("hva er høyden?"))
+    print(lengde*bredde*høyde)
+
+
+
+volum()

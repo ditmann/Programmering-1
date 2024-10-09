bf321f4b7e18cf6a8859fee35404bb2ac7a592ee
Adrian Ditmansen <adriandenutrolige@gmail.com>
2024-10-09 19:14:39 +0200

    ferdig med oppgave1

--- Oblig 3/oppgave1.py
+++ Oblig 3/oppgave1.py
@@ -1,8 +1,20 @@
-favoriteSpråk = {
-    "Adrian": "rust",
-    "Robin" : "Norsk",
-    "kåre" : "svensk",      
-    }
+#opretter info om studenten
+student = { 
+    "first name" : "Ola", 
+    "last name" : "Nordmann",
+    "favourite course" : "Programmering 1" 
+} 
 
-for folk in favoriteSpråk:
-    print(favoriteSpråk[folk])
+
+#printer ut first name og last name
+print(f"studenten heter: {student['first name']} {student["last name"]}")
+
+
+#legger til fagkode til programmering 1 og printer ut
+student["favourite course"] = "ITF10219 "+student["favourite course"]
+print(student["favourite course"])
+
+
+#legger til alderen til Ola og skriver den ut
+student ["age"] = 32
+print(f"ola er {student['age']} år gammel")

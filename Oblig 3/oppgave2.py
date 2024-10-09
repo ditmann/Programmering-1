ef019880eeedabfb9f04151ed124fe37d99aaa52
Adrian Ditmansen <adriandenutrolige@gmail.com>
2024-10-09 19:22:15 +0200

    Ferdig med oppgave2

---
+++ Oblig 3/oppgave2.py
@@ -0,0 +1,19 @@
+import random
+
+#funksjon som skriver ut et tilfeldig tall
+def getNumber():
+    print(f"""
+          ¤¤¤¤¤¤¤¤¤¤
+          ¤¤¤ {random.randrange(0,100)} ¤¤¤
+          ¤¤¤¤¤¤¤¤¤¤""")
+    
+
+getNumber()
+getNumber()
+getNumber()
+getNumber()
+getNumber()
+getNumber()
+getNumber()
+getNumber()
+getNumber()

84033d3af9167f9dbe617a934799f73eda7f0e4e
Adrian Ditmansen <adriandenutrolige@gamil.com>
2024-10-21 12:13:48 +0200

    startet på oblig 4

---
+++ Oblig 4/Oppgave2.py
@@ -0,0 +1,20 @@
+print("\u2665")
+print("\u2663")
+print("\u2666")
+print("\u2660")
+
+print("""
+-----
+| \u2665 |
+| 5 |
+-----         
+""")
+
+hor_line = '\u2500'
+vert_line = '\u2502'
+bottom_left_corner = '\u2514'
+upper_left_corner = '\u250C'
+bottom_right_corner = '\u2518'
+upper_right_corner = '\u2510'
+
+print(hor_line+hor_line+upper_right_corner)

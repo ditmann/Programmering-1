a148c3f679619e8f3e61aaac31e165e4d60fcf4a
Adrian Ditmansen <adriandenutrolige@gamil.com>
2024-11-12 11:00:43 +0100

    Lagt til masse bilder, lunsj

--- Arv og GUI Forelesning/PlanetVekt07.py
+++ Arv og GUI Forelesning/PlanetVekt07.py
@@ -4,8 +4,10 @@ from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QPixmap, QIcon
 
 
-planter = ["Merkur", "Venus", "Jorden", "Mars", "Jupiter", "Saturn", "Uranus", "Neptun"]
+planeter = ["Merkur", "Venus", "Jorden", "Mars", "Jupiter", "Saturn", "Uranus", "Neptun"]
 tyngdegraft = [3.7,8.87,9.807,3.721,24.79,10.44,8.87,11.15]
+planetBilder = ["Merkur.png", "Venus.png", "Jorden.png", "Mars.png", "Jupiter.png", "Saturn.png", "Uranus.png", "Neptun.png"]
+
 
 class Hovedvindu(QWidget):
     def __init__(self):
@@ -21,7 +23,44 @@ class Hovedvindu(QWidget):
         self.topplabel.setText("Hva er din vekt på andre planeter")
         self.layout.addWidget(self.topplabel,0,0,1,2, alignment = Qt.AlignmentFlag.AlignCenter)
 
+        self.skjema = QFormLayout()
+        self.menyCombobox = QComboBox()
+        self.menyCombobox.setPlaceholderText("Velg en planet")
+        self.menyCombobox.addItem("Tifeldig planet")
+        for planet in planeter:
+            self.menyCombobox.addItem(planet)
+
+        self.menyCombobox.activated.connect(self.oppdaterBilde())
+        self.skjema.addRow(self.menyCombobox)
+
+        
+
+        self.vektInput = QDoubleSpinBox()
+        self.vektInput.setPrefix("Din vekt i KG:                           ")
+        self.vektInput.setDecimals(1)
+        self.skjema.addRow(self.vektInput)
+
+        self.regnutknapp = QPushButton("Regn ut")
+        self.skjema.addRow(self.regnutknapp)
+
+
+        self.layout.addLayout(self.skjema,1,0)
+
+        self.bildelabel = QLabel()
+        self.pixmap = QPixmap("sun.png")
+        self.pixmap = self.pixmap.scaled(256,256)
+        self.bildelabel.setPixmap(self.pixmap)
+
+        self.layout.addWidget(self.bildelabel, 1,1)
+
+        self.bunnLabel= QLabel("Velg en planet og skriv inn vekten din!")
+        self.layout.addWidget(self.bunnLabel,2,0,1,2, alignment=Qt.AlignmentFlag.AlignHCenter)
+
         self.show()
+    
+    def oppdaterBilde(self):
+        self.pixmap = QPixmap
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)

7f759c2989fc39d536f64d0791142ef575eaaf7a
Adrian Ditmansen <adriandenutrolige@gmail.com>
2024-11-13 20:56:34 +0100

    :sparkles: lagt til 2 funksjoner i oblig 5

---
+++ Oblig 5/webshop.py
@@ -0,0 +1,46 @@
+def print_ware_information(ware):
+    for info in ware:
+        print(info)
+
+def calculate_average_ware_rating(ware):
+    '''Returnerer den gjennomsnittlige ratingen for en spesifisert vare.'''
+
+def get_all_wares_in_stock(all_wares):
+    stock = {}
+    for wares, info in all_wares.items():
+        if info["number_in_stock"] > 0:
+            stock[wares] = info
+    return stock
+
+
+def is_number_of_ware_in_stock(ware, number_of_ware):
+    '''Returnerer en Boolean-verdi som representerer om et spesifisert antall av
+    en gitt vare finnes på lager.'''
+
+def add_number_of_ware_to_shopping_cart(ware_key, ware, shopping_cart, number_of_ware):
+    '''Legger til et spesifisert antall av en gitt vare i en spesifisert
+    handlevogn.'''
+
+def calculate_shopping_cart_price(shopping_cart, all_wares, tax):
+    '''Returnerer prisen av en handlevogn basert på varene i den.'''
+
+def can_afford_shopping_cart(shopping_cart_price, wallet):
+    '''Returnerer en Boolean-verdi basert på om det er nok penger i en gitt
+    lommebok for å kjøpe en handlevogn.'''
+
+def buy_shopping_cart():
+    '''Kjøper varene i en handlevogn. Parameterene defineres i oppgaven.'''
+
+#------------------------------------------
+# Predefinerte funksjoner
+#------------------------------------------
+def is_ware_in_stock(ware):
+    '''Returnerer en Boolean-verdi som representerer om en vare er på lager.'''
+    if ware["number_in_stock"] >= 1:
+        return True
+    else:
+        return False
+
+def clear_shopping_cart(shopping_cart):
+    '''Tømmer en handlevogn.'''
+    shopping_cart.clear()
--- Test/test.py
+++ Test/test.py
@@ -1,6 +1,36 @@
-numbers = []
-for x in range(5):
-    numbers.insert(x,0)
+all_wares = {
+    "amd_processor": {
+        "name": "AMD Ryzen 9 5900X Processor",
+        "price": 5590.0,
+        "number_in_stock": 50,
+        "ratings": [4.5, 4.0, 5.0, 5.0, 4.5, 3.0],
+        "description": "All the cores and threads you'll need!",
+    },
+    "playstation_5": {
+        "name": "PlayStation 5",
+        "price": 5999.0,
+        "number_in_stock": 0,
+        "ratings": [5.0, 5.0, 4.5, 2.0, 5.0, 4.5, 4.0],
+        "description": "Next generation console, never in stock!",
+    },
+    "hdmi_cable": {
+        "name": "Belkin Ultra High Speed HDMI Cable - 2m",
+        "price": 349.0,
+        "number_in_stock": 3,
+        "ratings": [5.0, 5.0, 4.5, 5.0, 5.0, 5.0],
+        "description": "A high speed overprices HDMI cable!",
+    }
+}
 
 
-print(numbers)
+stock = {}
+for wares, info in all_wares.items():
+        if info["number_in_stock"] > 0:
+            stock[wares] = info
+    
+     
+      
+
+print (f"{stock}\n")
+
+#print (all_wares)
